7d875e37f32fb32057bd6dc676030434f712b1bf
Sinisa Panic <sinisa.panic89@gmail.com>
2023-11-22 09:16:57 +0100

    A minor bugfix in header file

--- HeaderAndFunctions/Exercise_CompareTwoNumbers/Lib.h
+++ HeaderAndFunctions/Exercise_CompareTwoNumbers/Lib.h
@@ -1,6 +1,8 @@
 #ifndef LIB_H
 #define LIB_H
 
+#include <stdint.h>
+
 int32_t largerOfTwoNumbers(int32_t number1, int32_t number2);
 int32_t smallerOfTwoNumbers(int32_t number1, int32_t number2);
 float averageOfTwoNumbers(int32_t number1, int32_t number2);
